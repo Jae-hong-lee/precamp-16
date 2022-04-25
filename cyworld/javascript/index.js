dd5812729df6b4f2b32026e74f2f227b82b3c8c6
Jae-hong-Lee <keash8091@gmail.com>
2022-04-25 22:11:18 +0900

    홈 게임 쥬크박스 버튼 수정

--- cyworld/javascript/index.js
+++ cyworld/javascript/index.js
@@ -1,21 +1,21 @@
 // menuHome
 function menuHome() {
     document.getElementById("contentFrame").setAttribute('src','home.html')
-    document.getElementById(menuHome).style = "color: black; background-color: white;"
-    document.getElementById(menuGame).style = "color: white; background-color: #298BE5;"
-    document.getElementById(menuJukebox).style = "color: white; background-color: #298BE5;"
+    document.getElementById("menuHome").style ="color: black; background-color: white;"
+    document.getElementById("menuGame").style ="color: white; background-color: #298BE5;"
+    document.getElementById("menuJukebox").style ="color: white; background-color: #298BE5;"
 }
 // menuGame
 function menuGame() {
     document.getElementById("contentFrame").setAttribute('src','game.html')
-    document.getElementById(menuGame).style = "color: black; background-color: white;"
-    document.getElementById(menuHome).style = "color: white; background-color: #298BE5;"
-    document.getElementById(menuJukebox).style = "color: white; background-color: #298BE5;"
+    document.getElementById("menuGame").style = "color: black; background-color: white;"
+    document.getElementById("menuHome").style = "color: white; background-color: #298BE5;"
+    document.getElementById("menuJukebox").style = "color: white; background-color: #298BE5;"
 }
 // menuJukebox
 function menuJukebox() {
     document.getElementById("contentFrame").setAttribute('src','jukebox.html')
-    document.getElementById(menuJukebox).style = "color: black; background-color: white;"
-    document.getElementById(menuGame).style = "color: white; background-color: #298BE5;"
-    document.getElementById(menuHome).style = "color: white; background-color: #298BE5;"
+    document.getElementById("menuJukebox").style = "color: black; background-color: white;"
+    document.getElementById("menuGame").style = "color: white; background-color: #298BE5;"
+    document.getElementById("menuHome").style = "color: white; background-color: #298BE5;"
 }
